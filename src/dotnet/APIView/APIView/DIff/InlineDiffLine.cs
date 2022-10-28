967ef236a9b2d4f01ae7adfb3e613b2f2a84e424
Chidozie Ononiwu <chononiw@microsoft.com>
2022-10-28 00:35:55 -0700

    Tidy up Heading of sections with diff

--- src/dotnet/APIView/APIView/DIff/InlineDiffLine.cs
+++ src/dotnet/APIView/APIView/DIff/InlineDiffLine.cs
@@ -4,23 +4,25 @@ namespace APIView.DIff
 {
     public struct InlineDiffLine<T>
     {
-        public InlineDiffLine(T line, DiffLineKind kind)
+        public InlineDiffLine(T line, DiffLineKind kind, bool isHeadingWithDiffInSection = false)
         {
             Line = line;
             Kind = kind;
-            OtherLine = default;
+            IsHeadingWithDiffInSection = isHeadingWithDiffInSection;
         }
 
-        public InlineDiffLine(T lineA, T lineB, DiffLineKind kind)
+        public InlineDiffLine(T lineA, T lineB, DiffLineKind kind, bool isHeadingWithDiffInSection = false)
         {
             Line = lineA;
             OtherLine = lineB;
             Kind = kind;
+            IsHeadingWithDiffInSection = isHeadingWithDiffInSection;
         }
 
         public T Line { get; }
-        public T OtherLine { get; }
+        public T OtherLine { get; } = default;
         public DiffLineKind Kind { get; }
+        public bool IsHeadingWithDiffInSection { get; }
 
         public override string ToString()
         {
