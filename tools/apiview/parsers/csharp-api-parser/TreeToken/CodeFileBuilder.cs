d8129dbd15e7cb5c2b2c20726e0020199c7eacbc
Chidozie Ononiwu <chononiw@microsoft.com>
2024-06-07 10:26:58 -0700

    Update Parser Version for CodeFile

--- tools/apiview/parsers/csharp-api-parser/TreeToken/CodeFileBuilder.cs
+++ tools/apiview/parsers/csharp-api-parser/TreeToken/CodeFileBuilder.cs
@@ -47,7 +47,7 @@ namespace CSharpAPIParser.TreeToken
 
         public ICodeFileBuilderSymbolOrderProvider SymbolOrderProvider { get; set; } = new CodeFileBuilderSymbolOrderProvider();
 
-        public const string CurrentVersion = "26";
+        public const string CurrentVersion = "27";
 
         private IEnumerable<INamespaceSymbol> EnumerateNamespaces(IAssemblySymbol assemblySymbol)
         {
